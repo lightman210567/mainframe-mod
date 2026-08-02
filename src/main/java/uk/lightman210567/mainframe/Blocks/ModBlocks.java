0f4e350d83d59be69d6f3c1e938bb7b6882bf24b
lightman210567 <81330314+lightman210567@users.noreply.github.com>
2026-08-02 15:38:09 +0100

    Added the ServerBlock
    Created a new block that will later hold the server functionality
    Created the items registry, to hold items and BlockItems
    Created a helper class to register all mod items (ModItems)
    
    Signed-off-by: lightman210567 <81330314+lightman210567@users.noreply.github.com>
    
    modified:   src/main/java/uk/lightman210567/mainframe/Blocks/ModBlocks.java
    new file:   src/main/java/uk/lightman210567/mainframe/Blocks/ServerBlock.java
    new file:   src/main/java/uk/lightman210567/mainframe/Items/ModItems.java
    modified:   src/main/java/uk/lightman210567/mainframe/Mainframe.java

--- src/main/java/uk/lightman210567/mainframe/Blocks/ModBlocks.java
+++ src/main/java/uk/lightman210567/mainframe/Blocks/ModBlocks.java
@@ -1,6 +1,9 @@
 package uk.lightman210567.mainframe.Blocks;
 
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import uk.lightman210567.mainframe.Mainframe;
 
@@ -9,7 +12,12 @@ public class ModBlocks {
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(Mainframe.MODID);
 
     // Register blocks here!
+    public static final DeferredBlock<Block> SERVER_BLOCK = BLOCKS.register(
+            "server_block",
+            () -> new ServerBlock(BlockBehaviour.Properties.of())
 
+    );
+    // END BLOCK REGISTER
 
     // Finalise block registration
     public static void registerBlocks(IEventBus modEventBus) {
---
+++ src/main/java/uk/lightman210567/mainframe/Blocks/ServerBlock.java
@@ -0,0 +1,11 @@
+package uk.lightman210567.mainframe.Blocks;
+
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import org.apache.logging.log4j.core.jmx.Server;
+
+public class ServerBlock extends Block {
+    public ServerBlock(BlockBehaviour.Properties properties) {
+        super(properties);
+    }
+}

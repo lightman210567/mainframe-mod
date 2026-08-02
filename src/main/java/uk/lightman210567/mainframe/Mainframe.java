4881388f77fe84fa3cb2c89609563311d8a101c4
lightman210567 <81330314+lightman210567@users.noreply.github.com>
2026-08-02 18:27:56 +0100

    Moved BlockItem logic into the blocks folder
    Moved the ModItems helper class into the Blocks folder, and renamed it to ModBlockItems
    This will help the distinction between items and BlockItem(s) when items get implemented (eg hard drives)
    
    Signed-off-by: lightman210567 <81330314+lightman210567@users.noreply.github.com>
    
    renamed:    src/main/java/uk/lightman210567/mainframe/Items/ModItems.java -> src/main/java/uk/lightman210567/mainframe/Blocks/ModBlockItems.java
    modified:   src/main/java/uk/lightman210567/mainframe/Mainframe.java

--- src/main/java/uk/lightman210567/mainframe/Mainframe.java
+++ src/main/java/uk/lightman210567/mainframe/Mainframe.java
@@ -1,38 +1,11 @@
 package uk.lightman210567.mainframe;
 
-import org.slf4j.Logger;
-
-import com.mojang.logging.LogUtils;
-
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
-import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
-import net.neoforged.neoforge.event.server.ServerStartingEvent;
-import net.neoforged.neoforge.registries.DeferredBlock;
-import net.neoforged.neoforge.registries.DeferredHolder;
-import net.neoforged.neoforge.registries.DeferredItem;
-import net.neoforged.neoforge.registries.DeferredRegister;
 import uk.lightman210567.mainframe.Blocks.ModBlockEntities;
 import uk.lightman210567.mainframe.Blocks.ModBlocks;
-import uk.lightman210567.mainframe.Items.ModItems;
+import uk.lightman210567.mainframe.Blocks.ModBlockItems;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(Mainframe.MODID)
@@ -45,7 +18,7 @@ public class Mainframe {
     // Call registration functions from here
     public Mainframe(IEventBus modEventBus, ModContainer modContainer) {
         ModBlocks.registerBlocks(modEventBus);
-        ModItems.registerItems(modEventBus);
+        ModBlockItems.registerItems(modEventBus);
         ModBlockEntities.registerBlockEntities(modEventBus);
     }
 }

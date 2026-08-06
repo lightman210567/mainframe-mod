c96d5724a1b4c9ca12e747cd9b0c65f26d35a454
lightman210567 <81330314+lightman210567@users.noreply.github.com>
2026-08-06 23:39:15 +0100

    added documentation for hard drive interaction
    Documented how to create methods for interaction with the hard drive item
    
    Signed-off-by: lightman210567 <81330314+lightman210567@users.noreply.github.com>
    
    Changes to be committed:
        modified:   src/main/java/uk/lightman210567/mainframe/Items/HardDrive.java

--- src/main/java/uk/lightman210567/mainframe/Items/HardDrive.java
+++ src/main/java/uk/lightman210567/mainframe/Items/HardDrive.java
@@ -1,9 +1,22 @@
 package uk.lightman210567.mainframe.Items;
 
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import uk.lightman210567.mainframe.Utility.ItemStackContainer;
 
 public class HardDrive extends Item {
     public HardDrive(Properties properties) {
         super(properties);
     }
+
+    public static final int size = 1;
+
+    // When you want to interact with the hard drive, create a method for that interaction
+    // This method will create an instance of the ItemStackContainer class
+    // Allowing you to interact with the container
+    // Call setChanged() to save the changes you made to the container
+    // You will need an individual method for each interaction
+
+    // ItemStack stack will be passed as a parameter into the method
+    // ItemStackContainer container = new ItemStackContainer(stack, size)
 }

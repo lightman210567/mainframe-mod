1196c3458ce020e359bdb25bcbdb1e480288500d
lightman210567 <81330314+lightman210567@users.noreply.github.com>
2026-08-07 13:24:34 +0100

    Added additional documentation to ItemStackContainer
    Documented the arguments on the ItemStackContainer class
    This will avoid confusion in the future
    
    Signed-off-by: lightman210567 <81330314+lightman210567@users.noreply.github.com>
    
    Changes to be committed:
    	modified:   src/main/java/uk/lightman210567/mainframe/Utility/ItemStackContainer.java

--- src/main/java/uk/lightman210567/mainframe/Utility/ItemStackContainer.java
+++ src/main/java/uk/lightman210567/mainframe/Utility/ItemStackContainer.java
@@ -8,8 +8,11 @@ import net.minecraft.world.item.component.ItemContainerContents;
 public class ItemStackContainer extends SimpleContainer {
     protected final ItemStack stack;
 
-    public ItemStackContainer(ItemStack stack, int size) {
-        super(size);
+    // the stack argument should be the ItemStack of the item the container is attached to
+    // eg if the container is for a disk item, you would pass the disk's ItemStack in
+    // the slotCount argument is the size of the container, measured in slots
+    public ItemStackContainer(ItemStack stack, int slotCount) {
+        super(slotCount);
         this.stack = stack;
         ItemContainerContents contents = stack.getOrDefault(DataComponents.CONTAINER, ItemContainerContents.EMPTY);
         contents.copyInto(this.getItems());

a9f3a5f42f0ff267aebcfcf4b10a929214a1ff47
lightman210567 <81330314+lightman210567@users.noreply.github.com>
2026-08-28 15:08:30 +0100

    Added default AbstractContainerMenu implementations
    Added the default required methods and super to the InterfaceItemMenu class
    These methods will need to be implemented properly in the future; they return null currently
    
    Implemented the super to the InterfaceItemMenu class according to neoforge docs
    
    Signed-off-by: lightman210567 <81330314+lightman210567@users.noreply.github.com>
    
    Changes to be committed:
    	modified:   src/main/java/uk/lightman210567/mainframe/Menu/InterfaceItemMenu.java

--- src/main/java/uk/lightman210567/mainframe/Menu/InterfaceItemMenu.java
+++ src/main/java/uk/lightman210567/mainframe/Menu/InterfaceItemMenu.java
@@ -1,7 +1,27 @@
 package uk.lightman210567.mainframe.Menu;
 
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.MenuType;
+import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.Nullable;
 
 public class InterfaceItemMenu extends AbstractContainerMenu {
 
+    protected InterfaceItemMenu(int containerId, Inventory playerInventory) {
+        super(ModMenus.INTERFACE_ITEM_MENU.get(), containerId);
+    }
+
+    // TODO Implement the quickMoveStack method
+    @Override
+    public ItemStack quickMoveStack(Player player, int i) {
+        return null;
+    }
+
+    // TODO Implement the stillValid method
+    @Override
+    public boolean stillValid(Player player) {
+        return false;
+    }
 }

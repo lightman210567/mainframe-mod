4df750c430ec90d346fedd9356ee303f9b2413bb
lightman210567 <81330314+lightman210567@users.noreply.github.com>
2026-08-28 16:03:45 +0100

    Created the server and client menu constructors
    In the InterfaceItemMenu class:
     - Created the server constructor, where all important setup code will happen
     - Created the client constructor, which has the job of getting the block entity and calling the server constructor
    
    Signed-off-by: lightman210567 <81330314+lightman210567@users.noreply.github.com>
    
    Changes to be committed:
    	modified:   src/main/java/uk/lightman210567/mainframe/Menu/InterfaceItemMenu.java

--- src/main/java/uk/lightman210567/mainframe/Menu/InterfaceItemMenu.java
+++ src/main/java/uk/lightman210567/mainframe/Menu/InterfaceItemMenu.java
@@ -1,16 +1,28 @@
 package uk.lightman210567.mainframe.Menu;
 
+import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.Nullable;
+import uk.lightman210567.mainframe.Blocks.Entity.ServerBlockEntity;
+import uk.lightman210567.mainframe.Items.InterfaceItem;
 
 public class InterfaceItemMenu extends AbstractContainerMenu {
 
-    protected InterfaceItemMenu(int containerId, Inventory playerInventory) {
+    // Server constructor
+    // All important menu setup happens here
+    public InterfaceItemMenu(int containerId, Inventory playerInventory, ServerBlockEntity serverBlockEntity) {
         super(ModMenus.INTERFACE_ITEM_MENU.get(), containerId);
+    }
+
+    // Client constructor
+    // It's only job is to get the block entity and call the server constructor
+    // Gets the block entity from the block position in the FriendlyByteBuf
+    public InterfaceItemMenu(int containerId, Inventory playerInventory, FriendlyByteBuf byteBuf) {
+        this(containerId, playerInventory, (ServerBlockEntity) playerInventory.player.level().getBlockEntity(byteBuf.readBlockPos()));
     }
 
     // TODO Implement the quickMoveStack method
